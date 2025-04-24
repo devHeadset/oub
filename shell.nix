7f9493f16ac45fec579d356c00ae06298430264f
devHeadset <headset@devheadset.org>
2025-04-24 17:05:24 -0400

    adding shell.nix (again)

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{
+  pkgs ? import <nixpkgs> {}
+}:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.python311Packages.setuptools
+    pkgs.python311Packages.wheel
+    pkgs.python311Packages.aiohttp
+    pkgs.python311Packages.python-dotenv
+  ];
+
+  shellHook = ''
+    echo "setting up virtual environment..."
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install --upgrade pip
+    pip install discord.py
+    echo "environment ready."
+  '';
+}
